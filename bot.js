bca1eb7383a7fb84603815ac2e33cc18e7e834ac
epic3azoozez135 <44898336+epic3azoozez135@users.noreply.github.com>
2018-11-14 18:39:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
-var prefix = "l"
+var prefix = "A"
 
 
 
@@ -58,4 +58,4 @@ client.on('message', function(message) {
     }
 });
 
-client.login(process.env.BOT_TOKEN100);
+client.login(process.env.BOT_TOKEN);
